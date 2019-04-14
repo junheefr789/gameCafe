6da5e92f6922064bb84a9bd9142c6061db840534
junhe <junhe@DESKTOP-24QC5TT>
2019-04-15 02:51:51 +0900

    second

---
+++ project/src/main/java/com/taeyoung/project/onecard/onecardController.java
@@ -0,0 +1,19 @@
+package com.taeyoung.project.onecard;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class onecardController {
+
+	@RequestMapping(value = "/onecardInvite.go", method = RequestMethod.GET)
+	public String onecardInvite(HttpServletRequest request) {
+		request.setAttribute("logInBox", "member/logBefore.jsp");
+		request.setAttribute("contentPage", "onecard/gameInvite.jsp");
+		return "index";
+	}
+
+}
--- project/src/main/webapp/WEB-INF/views/onecard/gameInvite.jsp
+++ project/src/main/webapp/WEB-INF/views/onecard/gameInvite.jsp
@@ -7,7 +7,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 <script type="text/javascript">
-	$(document).ready(function(){
+	$(function(){
 		$("#writeButton").click(function(){
 			sendMessage();
 			$("#writeText").val("");
